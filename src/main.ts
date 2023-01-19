f68164276be4053819b0afab49888ea0fafde821
나진주 <jinju@liveklass.com>
2023-01-19 15:33:34 +0900

    test commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // test code
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
