d31f0e424cef5c25383256d695b5615d47f4e25c
나진주 <jinju@liveklass.com>
2023-01-25 15:34:37 +0900

    login and user info page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import { Get, Controller, Render, Post, Body, Param } from '@nestjs/common';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root() {
+    return { message: 'Hello world!' };
+  }
+
+  @Post('/submit')
+  submitForm(@Body('id') id: string, @Body('pw') pw: string) {
+    console.log('id, pw', id, pw);
+    if (!id || !pw) {
+      return {
+        result: false,
+        message: '아이디와 비밀번호를 입력 해 주세요.',
+      };
+    }
+    return {
+      result: true,
+      user: { id: id, name: '진주', age: 20, nickName: 'dindoo' },
+    };
+  }
+
+  @Get('/info/:id')
+  @Render('info')
+  info(@Param('id') id: string) {
+    return { user: { id: id, name: '진주', age: 20, nickname: 'dindoo' } };
   }
 }
